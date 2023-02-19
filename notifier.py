73b83a596eb21acedf4ea9ae4373c269e9acbc24
avery <75399033+overworded@users.noreply.github.com>
2023-02-19 14:49:37 -0600

    Update notifier.py

--- notifier.py
+++ notifier.py
@@ -2,10 +2,10 @@ import requests
 import time
 from datetime import datetime
 
-RobloxCatalog = "https://catalog.roblox.com/v1/search/items?category=All&creatorName=Roblox&includeNotForSale=true&limit=120&salesTypeFilter=1&sortType=3"
-UGCCatalog = "https://catalog.roblox.com/v1/search/items?category=Accessories&includeNotForSale=true&limit=120&salesTypeFilter=1&sortType=3&subcategory=Accessories"
-RobloxWebhook = "https://discord.com/api/webhooks/..."
-UGCWebhook = "https://discord.com/api/webhooks/..."
+RobloxCatalog = "https://catalog.roblox.com/v1/search/items?category=All&creatorName=Roblox&includeNotForSale=true&limit=120&salesTypeFilter=1&sortType=3" # catalog link for items created by the Roblox account.
+UGCCatalog = "https://catalog.roblox.com/v1/search/items?category=Accessories&includeNotForSale=true&limit=120&salesTypeFilter=1&sortType=3&subcategory=Accessories" # catalog link for items created by users.
+RobloxWebhook = "https://discord.com/api/webhooks/..." # Webook url for the Roblox items.
+UGCWebhook = "https://discord.com/api/webhooks/..." # Webook url for the user generated items.
 
 def fetchItems(api):
     response = requests.get(api)
@@ -18,11 +18,11 @@ def fetchItems(api):
 previousRobloxItems = fetchItems(RobloxCatalog)
 previousUGCItems = fetchItems(UGCCatalog)
 
-def printWithTS(text):
+def printWithTS(text): # i like timestamps.
     stamp = datetime.now().strftime("%H:%M:%S")
     return print(f"[{stamp}] - {text}")
 
-def thumbnail(itemid):
+def thumbnail(itemid): # getting the thumbnail of the item from roblox because they suck!
     url = f"https://thumbnails.roblox.com/v1/assets?assetIds={itemid}&size=250x250&format=Png"
     response = requests.get(url)
     imageUrl = response.json()["data"][0]["imageUrl"]
@@ -40,7 +40,7 @@ def compareItems(currentItems, previousItems, webhook):
         for item in addedItems:
             details = itemDetails(item['id'])
             printWithTS(f"ID: {item['id']}, Type: {item['itemType']}")
-            requests.post(webhook, json={"embeds": [{
+            requests.post(webhook, json={"embeds": [{ # discord webhook info.
                         "title": details['Name'],
                         "url": f"https://www.roblox.com/catalog/{item['id']}",
                         "fields": [
@@ -50,15 +50,13 @@ def compareItems(currentItems, previousItems, webhook):
                         ],
                         "color": 0x84FF9B,
                         "thumbnail": {"url": thumbnail(item['id'])},
-                        "footer": {"text": details['Creator']['Name']},
-                        "timestamp": details['Created']
                 }]})
     return currentItems
 
 def scan():
     while True:
         printWithTS("Checking for new items in 15 seconds!")
-        time.sleep(15)  # wait for 5 seconds before checking again
+        time.sleep(15)  # wait so you dont get ratelimited.
         global previousRobloxItems
         global previousUGCItems
         previousRobloxItems = compareItems(fetchItems(RobloxCatalog), previousRobloxItems, RobloxWebhook)
